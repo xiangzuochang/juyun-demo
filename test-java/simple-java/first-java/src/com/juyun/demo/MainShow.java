b11a60cba9e07f8912a6368d48c7158f03306f13
bwilling <bwilling@126.com>
2020-04-24 15:25:09 +0800

    add java code

---
+++ test-java/simple-java/first-java/src/com/juyun/demo/MainShow.java
@@ -0,0 +1,10 @@
+package com.juyun.demo;
+
+public class MainShow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is first java show");
+	}
+
+}
